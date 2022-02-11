c0011b5fdd7819e74284c2c802279afde91530b2
DdooBuggy <yhiyo03@gmail.com>
2022-02-11 16:37:59 +0900

    submitted assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const purple = "color-purple";
+const yello = "color-yello";
+const blue = "color-blue";
+const body = document.querySelector("body");
+
+function changeColor() {
+    if (window.innerWidth < 1000) {
+        body.className = "";
+        body.classList.add(blue);
+    } else if (window.innerWidth > 1500) {
+        body.className = "";
+        body.classList.add(yello);
+    } else {
+        body.className = "";
+        body.classList.add(purple);
+    }
+}
+window.addEventListener("resize", changeColor);
